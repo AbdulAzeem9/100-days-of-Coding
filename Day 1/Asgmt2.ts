668ccc0c619fd548f1fe0ecf1e95a62ead591f48
AbdulAzeem9 <162511682+AbdulAzeem9@users.noreply.github.com>
2024-03-18 00:53:08 +0500

    Add files via upload

---
+++ Day 1/Asgmt2.ts
@@ -0,0 +1,3 @@
+let person: string = "Azeem"; //This saves the name
+
+console.log(`Hello ${person}, would you like to learn some TypeScript Today?`); //This shows the message
---
+++ Day 1/Asgmt3.ts
@@ -0,0 +1,4 @@
+let pName :string = "Azeem"; // This saves the name
+console.log(pName.toLowerCase()); //shows name in small letters
+console.log(pName.toUpperCase()); //shows name in BIG LETTERS
+console.log(pName.charAt(0).toUpperCase() + pName.slice(1).toLowerCase()); //shows name with the first letter BIG
