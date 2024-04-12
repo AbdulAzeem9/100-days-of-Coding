85d6fb64f63ce31adc5bce5cf6e7a44a8d0a7765
AbdulAzeem9 <162511682+AbdulAzeem9@users.noreply.github.com>
2024-04-12 23:30:50 +0500

    Add files via upload

---
+++ Day 27/Asgmt79.ts
@@ -0,0 +1,10 @@
+// This object for a car with specific details 
+let car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: "2020"
+};
+
+// Accessing and showing the car's model
+console.log(car.model); // Outputs: Corolla 
+// We us dot notation (car.model) to get the model of the car from our object.
