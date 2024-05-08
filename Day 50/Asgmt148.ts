0b335966b2a384901d7122cf069b4acef131b768
AbdulAzeem9 <162511682+AbdulAzeem9@users.noreply.github.com>
2024-05-08 17:52:45 +0500

    Add files via upload

---
+++ Day 50/Asgmt148.ts
@@ -0,0 +1,6 @@
+// Uses setTimeout to log a message after a 2-second delay
+setTimeout(() => {
+    console.log("This message is shown after a 2-second delay.");
+  }, 4000);
+  // This showcases how to use setTimeout to delay actions in your code.
+
